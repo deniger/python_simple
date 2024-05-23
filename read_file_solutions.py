ab9fe8caf081bb4d68692e9df1287732bddc5684
Fred Deniger <fdeniger@icrc.org>
2024-05-23 17:59:37 +0200

    comment

--- read_file_solutions.py
+++ read_file_solutions.py
@@ -1,3 +1,4 @@
+# Voir doc: https://www.guru99.com/python-file-readline.html
 # pour lire toutes les lignes d'un fchier
 print("---------------------------")
 print("Solution qui ne fait pas de strip et donc avec les retours chariots")
@@ -20,4 +21,17 @@ while ligne!="":
     ligne=file.readline()
 file.close()
 print("fichier lu avec readLine et utilisant strip est \n"+str(tableau))
+
+
+print()
+print("---------------------------")
+print("il est possible de lire toutes les lignes d'un coup")
+
+file = open("album.txt",'r')
+lignes=file.readlines()
+tableau=[]
+for line in lignes:
+    tableau.append(line.strip())
+file.close()
+print("fichier lu d'un coupt\n"+str(tableau))
 
